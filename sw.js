95bde8647c6e5821730246b823e6d9befc302364
Christer Olsen <colsen1991@gmail.com>
2017-05-07 19:35:42 +0800

    Getting the site ready to be served from new domain.

--- sw.js
+++ sw.js
@@ -1,25 +1,27 @@
 (function () {
-  const CACHE_NAME = 'my-site-cache-v1';
+  const CACHE_NAME = 'beer-o-clock-clock-cache-v1';
   const urlsToCache = [
-    '/beer-o-clock-clock/',
-    '/beer-o-clock-clock/style/index.css',
-    '/beer-o-clock-clock/img/beer.png',
-    '/beer-o-clock-clock/img/favicon.ico',
-    '/beer-o-clock-clock/img/favicon-48.png',
-    '/beer-o-clock-clock/img/favicon-96.png',
-    '/beer-o-clock-clock/img/favicon-144.png',
-    '/beer-o-clock-clock/img/favicon-192.png',
-    '/beer-o-clock-clock/js/clock.js',
-    '/beer-o-clock-clock/js/error.js',
-    '/beer-o-clock-clock/js/data.js',
-    '/beer-o-clock-clock/js/index.js'
+    '/',
+    '/style/index.css',
+    '/img/beer.png',
+    '/img/favicon.ico',
+    '/img/favicon-48.png',
+    '/img/favicon-96.png',
+    '/img/favicon-144.png',
+    '/img/favicon-192.png',
+    '/js/clock.js',
+    '/js/error.js',
+    '/js/data.js',
+    '/js/index.js',
+    '/js/serviceWorker.js',
+    '/js/text.js',
+    '/js/tools.js'
   ];
 
   self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open(CACHE_NAME)
         .then(function (cache) {
-          console.log('Opened cache');
           return cache.addAll(urlsToCache);
         })
     );
@@ -30,11 +32,9 @@
       caches.match(event.request)
         .then(function (response) {
             if (response) {
-              console.log('hit, ret from cache');
               return response;
             }
 
-            console.log('miss, fetch from server');
             return fetch(event.request);
           }
         )
