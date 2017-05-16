5be195f04cd4e9f21a6f03c243c12f7dce7d5dc8
Christer Olsen <colsen1991@gmail.com>
2017-05-16 19:37:31 +0800

    changed visited previously visited logic to use cookie

--- js/swRegistration.js
+++ js/swRegistration.js
@@ -8,7 +8,8 @@ const initServiceWorker = (function () {
       window.addEventListener('load', () => {
         navigator.serviceWorker.getRegistrations()
           .then(registrations => {
-            if (!registrations[ 0 ]) {
+            if (!document.cookie.match('visited=1')) {
+              document.cookie = 'visited=1;';
               setTimeout(() => popup.show('<p>Website ready for offline use.<br>Also try "Menu" > "Add to home screen"</p>', 'message', 5000), 2000);
             }
           })
