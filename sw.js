f9b7eb82219cd764282ea9fecc01ad438abd2c11
Christer Olsen <colsen1991@gmail.com>
2017-06-21 14:14:47 +1000

    cached icons.

--- sw.js
+++ sw.js
@@ -5,7 +5,6 @@
     '/index.html',
     '/style/base.css',
     '/style/site.css',
-    '/img/beer.png',
     '/js/clock.js',
     '/js/data.js',
     '/js/error.js',
@@ -13,7 +12,21 @@
     '/js/popup.js',
     '/js/swreg.js',
     '/js/text.js',
-    '/js/tools.js'
+    '/js/tools.js',
+    '/img/beer.png',
+    '/img/apple-touch-icon.png',
+    '/img/favicon.ico',
+    '/img/favicon-16x16.png',
+    '/img/favicon-32x32.png',
+    '/img/favicon-48x48.png',
+    '/img/favicon-64x64.png',
+    '/img/favicon-96x96.png',
+    '/img/favicon-128x128.png',
+    '/img/favicon-144x144.png',
+    '/img/favicon-152x152.png',
+    '/img/favicon-192x192.png',
+    '/img/mstile-150x150.png',
+    '/img/safari-pinned-tab.svg',
   ];
 
   self.addEventListener('install', (event) => {
